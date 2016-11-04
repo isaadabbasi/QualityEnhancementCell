787950fb9cfc24e1bdf2a685b1d330d04cb8ff80
Saad Abbasi <itssaadabbasi@gmail.com>
2016-11-04 20:12:24 +0500

    Firebase integrated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+    _root: FirebaseObjectObservable <any> = null;
+    persons: FirebaseObjectObservable<any> = null;
+    item: FirebaseObjectObservable<any>;
+    title = 'app works!';
+
+    constructor(public af: AngularFire) {
+      this.item = af.database.object('/persons/');
+    }
+
+    save(newName: string) {
+      this.item.set({ phone: newName });
+    }
+
+    update(newSize: string) {
+      this.item.update({ address: newSize });
+    }
+
+    ngOnInit(){}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2'
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDGAd9mRNSP3mWXB_MdnwXxaeJsWKqqPgE",
+    authDomain: "qehancementc.firebaseapp.com",
+    databaseURL: "https://qehancementc.firebaseio.com",
+    storageBucket: "qehancementc.appspot.com",
+    messagingSenderId: "864675246842"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
     FormsModule,
     HttpModule
