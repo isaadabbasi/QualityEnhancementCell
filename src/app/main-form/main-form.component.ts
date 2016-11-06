48e34ad0252bde51c0b238c0c6ed892dc39bd299
Saad Abbasi <itssaadabbasi@gmail.com>
2016-11-07 00:31:23 +0500

    button added to question-card panel-header

--- src/app/main-form/main-form.component.ts
+++ src/app/main-form/main-form.component.ts
@@ -4,15 +4,45 @@ import { QuestionComponent } from './quest.component'
 
 @Component({
     selector: 'main-form',
-    template: `<quest *ngFor="let quest of questions"></quest>`,
+    template: `
+        <quest *ngFor="let quest of questions"
+        [percentage]="quest.percentage"
+        [question]="quest.question"
+        [_id]="quest.q_id"
+        ></quest>`,
     styleUrls: ['main.component.css']
 })
 
 export class MainFormComponent implements OnInit{
     constructor(){}
-    questions: Array<Number> = [1,2,3,4];
+    questions: Array<Object> = null;
     ngOnInit(){
-        console.log('main form initialized');
+        this.questions = [
+            {q_id: 1,
+            question:'Does he/she arrives and leave the class on time',
+            percentage: 88
+            },
+            {q_id: 2,
+            question:'Is he/she fair in examination and session marks',
+            percentage: 35
+            },
+            {q_id: 3,
+            question:'Do she/he provide some content other than course material to help the course content',
+            percentage: 80
+            },
+            {q_id: 4,
+            question:'He/She does any kind of gender, religious or linguistic discremination',
+            percentage: 56
+            },
+            {q_id: 5,
+            question:'Do she/he provide some content other than course material to help the course content',
+            percentage: 78
+            },
+            {q_id: 6,
+            question: 'Does he/she arrives and leave the class on time',
+            percentage: 22
+            },
+        ] 
     }
 
 }
--- src/app/main-form/quest.component.ts
+++ src/app/main-form/quest.component.ts
@@ -1,34 +1,52 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
     selector: 'quest',
     template: `
-    <div style="margin:10px 50px;" class="panel panel-primary">
-        <div style="padding:2px !important;" class="panel-heading">
+    <div style="margin:10px 50px;" class="panel panel-primary" id="_id">
+        <div style="padding:10px !important;" class="panel-heading">
+            <div class="pull-right">
+                <button class="btn-xs btn-danger" (click)="hideQuest(_id)">X</button>    
+            </div>
         </div>
         <div class="panel-body">
-            <div class="col-xs-3">
-                <h1 class="text-center">90%</h1>
+            <div class="col-md-3">
+                <h1 class="text-center" [ngClass]="{'text-danger': percentage<40, 
+                    'text-primary': percentage>=40 && percentage <=60,
+                    'text-success': percentage>60}">{{percentage}}%</h1>
             </div>
-            <div class="col-xs-9">
-                <p class="text-center">Does he/she arrives and leave the class lecture on time?</p>
-                <div class="text-center"> 
-                    <input type="radio" name="question1" value="Strongly Agree">Strongly Agree
-                    <input type="radio" name="question1" value="Agree">Agree
-                    <input type="radio" name="question1" value="Average">Average
-                    <input type="radio" name="question1" value="Disagree">Disagree
+            <div class="col-xs-12 col-md-9">
+                <p class="text-center">{{question}}</p> <hr>
+                <div class="text-center" style="margin:-10px auto"> 
+                    <input type="radio" name="question1" value="Strongly Agree">Strongly Agree &nbsp;
+                    <input type="radio" name="question1" value="Agree">Agree &nbsp;
+                    <input type="radio" name="question1" value="Average">Average &nbsp;
+                    <input type="radio" name="question1" value="Disagree">Disagree &nbsp;
                     <input type="radio" name="question1" value="Strongly Disagree">Strongly Disagree
                 </div>
             </div>
         </div>
-    </div>`,
+    </div>`
 })
 
 export class QuestionComponent implements OnInit{
     constructor(){}
 
+
+
+    @Input() question = null;
+    @Input() percentage = null;
+    @Input() _id = null;
+
     ngOnInit(){
         console.log('main form initialized');
+    }
+
+    minimizeQuest(){
+        console.log('minimize');
+    }
+    hideQuest(id: number){
+        console.log('close tab #'+id);
     }
     
 }
