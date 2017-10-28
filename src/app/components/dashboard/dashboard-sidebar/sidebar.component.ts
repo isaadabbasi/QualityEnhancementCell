4e19fc2aea33451b18d355dc5575c8aac4040c47
SMTaha <s.m.taha10@gmail.com>
2017-10-28 14:20:01 +0500

    Minor changes

--- src/app/components/dashboard/dashboard-sidebar/sidebar.component.ts
+++ src/app/components/dashboard/dashboard-sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -33,36 +34,38 @@ import { Component, OnInit } from '@angular/core';
     template: `
     <div class="sidebar">
           <ul class="nav nav-sidebar">
-            <a class="col-xs-12 col-md-12 side-option" title="Start Survey" routerLinkActive="active-link" routerLink="start">
+            <a *ngIf="user === 'student'" class="col-xs-12 col-md-12 side-option" title="Start Survey" routerLinkActive="active-link" routerLink="start">
                 <li class="sidenav-item">
                         <div class="fa fa-pencil fa-3x active"></div>
                         <div class="sidenav-text">Start Survey</div>
                 </li>
             </a>
-            <a  class="col-xs-12 col-md-12 side-option" title="View Surveys" routerLinkActive="active-link" routerLink="view-surveys">
-                <li class="sidenav-item">
-                        <div class="fa fa-check-square-o fa-3x active"></div>
-                        <div class="sidenav-text">View Surveys</div>
-                </li>
-            </a>
-             <a class="col-xs-12 col-md-12 side-option" title="Rankings" routerLinkActive="active-link" routerLink="rankings">
-                <li class="sidenav-item">
-                        <div class="fa fa-trophy fa-3x active"></div>
-                        <div class="sidenav-text">Rankings</div>
-                </li>
-            </a>
-            <a class="col-xs-12 col-md-12 side-option" title="Statistics" routerLinkActive="active-link" routerLink="stats">
-                <li class="sidenav-item">
-                        <div class="fa fa-line-chart fa-3x active"></div>
-                        <div class="sidenav-text">Statistics</div>
-                </li>
-            </a>
-            <a class="col-xs-12 col-md-12 side-option" title="Settings" routerLinkActive="active-link" routerLink="settings">
-                <li class=" sidenav-item">
-                        <div class="fa fa-gears fa-3x active"></div>
-                        <div class="sidenav-text">Settings</div>
-                </li>
-            </a>
+            <div *ngIf="user === 'admin'">
+                <a class="col-xs-12 col-md-12 side-option" title="View Surveys" routerLinkActive="active-link" routerLink="view-surveys">
+                    <li class="sidenav-item">
+                            <div class="fa fa-check-square-o fa-3x active"></div>
+                            <div class="sidenav-text">View Surveys</div>
+                    </li>
+                </a>
+                <a class="col-xs-12 col-md-12 side-option" title="Rankings" routerLinkActive="active-link" routerLink="rankings">
+                    <li class="sidenav-item">
+                            <div class="fa fa-trophy fa-3x active"></div>
+                            <div class="sidenav-text">Rankings</div>
+                    </li>
+                </a>
+                <a class="col-xs-12 col-md-12 side-option" title="Statistics" routerLinkActive="active-link" routerLink="stats">
+                    <li class="sidenav-item">
+                            <div class="fa fa-line-chart fa-3x active"></div>
+                            <div class="sidenav-text">Statistics</div>
+                    </li>
+                </a>
+                <a class="col-xs-12 col-md-12 side-option" title="Settings" routerLinkActive="active-link" routerLink="settings">
+                    <li class=" sidenav-item">
+                            <div class="fa fa-gears fa-3x active"></div>
+                            <div class="sidenav-text">Settings</div>
+                    </li>
+                </a>
+            </div>
             <a style="cursor: pointer; cursor: hand" class="col-xs-12 col-md-12 side-option" title="Logout" routerLinkActive="active-link" (click)="logout()">
             <li class=" sidenav-item">
                     <div class="fa fa-sign-out fa-3x active"></div>
@@ -71,11 +74,17 @@ import { Component, OnInit } from '@angular/core';
         </a>
           </ul>
         </div>`
-})
+})  
 export class DashboardSidebarComponent implements OnInit {
+    user: Object;
     constructor(private router: Router) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.user = JSON.parse(localStorage.getItem('activeUser'))
+                        .hasOwnProperty('email') ? 
+                        'admin' : 'student'
+        
+     }
 
     logout(){
         localStorage.clear();
