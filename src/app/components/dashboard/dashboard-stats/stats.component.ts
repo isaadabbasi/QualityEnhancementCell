9ae0117a7aaca4e69ca3fb666ec6e66e6e59d998
SMTaha <s.m.taha10@gmail.com>
2017-11-03 20:15:51 +0500

    Add filters to stats

--- src/app/components/dashboard/dashboard-stats/stats.component.ts
+++ src/app/components/dashboard/dashboard-stats/stats.component.ts
@@ -1,18 +1,25 @@
+import { Observable } from 'rxjs/Observable';
 import { OnInit } from '@angular/core';
-import { SURVEY_LIST, TEACHER_BASE_URL, TEACHER_DETAILS_URL } from './../../../shared/global-vars';
+import { SURVEY_LIST, TEACHER_BASE_URL, TEACHER_DETAILS_URL, Departments, TEACHER_DETAILS_BY_DEPARTMENT } from './../../../shared/global-vars';
 import { SharedService } from './../../../shared/shared.service';
 import { Component, ViewChild } from '@angular/core';
 import * as _ from "lodash";
 
 @Component({
     selector: 'stats',
-    // template: `
-    //     <div>Ranking will be rendered here</div> 
-    // `
-    templateUrl: './stats.template.html'
+    templateUrl: './stats.template.html',
+    styleUrls: ['./stats.css']
 })
 export class StatsComponent implements OnInit{
-  
+  teacherSurvey: {}[];
+  showLoader: boolean;
+  timeToFetch: number;
+  length: any;
+  surveysArray: any;
+  SurveyId: any;
+  teachersList: any;
+  showTeachersList: boolean;
+  deparmentsList = Departments;
   showDetails: Boolean = false;
     // lineChart
   public surveys = [
@@ -23,17 +30,15 @@ export class StatsComponent implements OnInit{
     // { data: [5,2,7,4,5,6,1,8,2,1], avg: 4.1, label: 'Survey 4'}
   ];
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
   ];    //Get data from server.
-  public lineChartLabels:Array<any> = ['Survey 1', 'Survey 2', 'Survey 3', 'Survey 4'];
+  public lineChartLabels:Array<any> = ['Survey 1'];
   public lineChartOptions:any = {
     responsive: true
   };
   
   public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';  //line, bar, radar, pie, polarArea, doughnut
+  public lineChartType:string = 'bar';  //line, bar, radar, pie, polarArea, doughnut
   public detailsChartType: string = 'bar'
   public randomize():void {
     let _lineChartData:Array<any> = new Array(this.lineChartData.length);
@@ -75,14 +80,73 @@ export class StatsComponent implements OnInit{
           
         }
       )
-      // survey.subscribe(next => console.log(next))
     this.options = {
       title: { text: 'simple chart'},
       series: [{
-        name: 's1',
-        data: [2,3,5,8,13],
+        name: 'Question No.',
+        data: [2, 3, 5, 8, 13],
         allowPointSelect: true
       }]
     }
+  }
+  getNextList(entity: string, value: string){
+    let URL = entity === 'teachers' && value != "0"? 
+      TEACHER_DETAILS_BY_DEPARTMENT + value 
+      :
+      '';
+    if(value != "0"){
+      this.showTeachersList = true;
+      console.log(URL, value, this.showTeachersList);
+      this.sharedService.getCall(URL)
+        .subscribe(
+          next => {
+            this.showTeachersList = true;
+            console.log(next)
+            this.teachersList = next["body"];
+          },
+          err => console.log(err),
+          () => console.log('complete')
+        )
+      }else{
+        this.showTeachersList = false;
+      }
+    }
+      
+  viewSurvey(id){
+    this.SurveyId.emit(id);  
+  }
+  showSurvey(teacherName: string){
+    
+    let selectedTeacher: Object = {};
+    let singleSurveys = [];
+    // console.time()
+    let start = Date.now()
+    if(teacherName !== '0'){
+      this.loaderState(true);
+      selectedTeacher = (this.teachersList.filter(teacher => teacher["fullname"] === teacherName))[0];
+      this.surveysArray = selectedTeacher["surveys"];
+      _.forEach(this.surveysArray, survey => {
+        singleSurveys.push(this.sharedService.getCall(`${SURVEY_LIST}/id/${survey._reference}`));
+        
+      });
+      
+      Observable.forkJoin(singleSurveys)
+        .subscribe(
+          result => {
+            console.log(result)
+            this.teacherSurvey = result;
+          },
+          err => console.log(err),
+          () => setTimeout(this.loaderState(false), 2500)
+        )
+      this.length = this.surveysArray.length;
+      console.log(this.surveysArray.length);
+    }
+    let end = Date.now();
+    this.timeToFetch = end - start;
+    console.log(this.timeToFetch, 'ms');
+  }
+  loaderState(hidden: boolean){
+    this.showLoader = hidden;
   }
 }
