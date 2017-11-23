22dbdbbd916ca000927a3f1b69777dd137584e63
isaadabbasi <itssaadabbasi@gmail.com>
2017-11-23 13:54:44 +0500

    sum of marks on each question, excel

--- server/routes/excel.route.js
+++ server/routes/excel.route.js
@@ -17,43 +17,54 @@ function transformIntoExcel(data){
         "score": sur.value
     }));
     
-
-    // Array.of(data.survey.)
-
-    // var myCars = [
-    //     {
-    //         "carModel": "Audi",
-    //         "selected": '0',
-    //         "score": ["blue","green","yellow"]
-    //     }, {
-    //         "carModel": "BMW",
-    //         "selected": '15000',
-    //         "score": ["red","blue"]
-    //     }, {
-    //         "carModel": "Mercedes",
-    //         "selected": '20000',
-    //         "score": "yellow"
-    //     }, {
-    //         "carModel": "Porsche",
-    //         "selected": '30000',
-    //         "score": ["green","teal","aqua"]
-    //     }
-    // ];
-    console.log(data.survey);
     var csv = json2csv({ data: survey, fields: fields, unwindPath: 'score' });
     return csv;
 }
 
 function printById(req, res){
-    let id = req.params.id;
-    SurveyJoint.getSurveyById(id)
-        .then(survey => {
-            let csv = transformIntoExcel(survey.body, SINGLE);
-            res.attachment('survey.xls');
-            res.status(200).send(csv);
+    let id = req.params && req.params.id;
+    let query = req.query;
+    console.log('checking params')
+    if(id)
+        SurveyJoint.getSurveyById(id)
+            .then(survey => {
+                let csv = transformIntoExcel(survey.body, SINGLE);
+                res.attachment('survey.xls');
+                res.status(200).send(csv);
+            })
+    else 
+        res.status(400).send('choose a record to get record for');
+}
+
+function printByQuery(req, res){
+    let query = req.query,
+        params = {};
+
+    query.fullname && (params.fullname = query.fullname);
+
+    // if(! Object.keys(params).length){
+    //     res.status(400).send('Must send some details to get record for...');
+    //     return;
+    // }
+    SurveyJoint.getAllSurveys(query)
+        .then(prores => {
+            let counter = [];
+            let body = prores.body
+            .map(surveys => surveys.survey
+                .filter(s=> typeof s.id === 'number'));
+                
+            for (let i=0; i<body.length; i++){
+                counter.push({ 1:0, 2:0, 3:0, 4:0, 5:0 })
+                for(let j=0; j<body[i].length; j++)
+                    counter[i][body[i][j].value]++;
+            }
+            console.log(counter);
+            res.status(prores.status).send(body);    
         })
+        .catch(console.error)
 }
 
 router.get('/:id', printById);
+router.get('/', printByQuery);
 
 module.exports = router;
