744cd6de43738c57178c6ce6d238a2951e96a59b
SMTaha <s.m.taha10@gmail.com>
2017-12-11 11:10:45 +0500

    Add modal

--- src/app/components/dashboard/dashboard-settings/settings.component.ts
+++ src/app/components/dashboard/dashboard-settings/settings.component.ts
@@ -1,13 +1,20 @@
-import { TEACHER_DETAILS_URL } from './../../../shared/global-vars';
+import { TEACHER_DETAILS_URL, Departments as DepartmentsList, TEACHER_BASE_URL } from './../../../shared/global-vars';
 import { SharedService } from './../../../shared/shared.service';
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef, ViewChild } from '@angular/core';
+import { ModalComponent } from '../../../modal/modal.component';
 @Component({
     selector: 'settings',
     templateUrl: './settings.html'
 })
 export class SettingsComponent {
+
+    @ViewChild('modal', {read: ViewContainerRef}) container: ViewContainerRef;
+    
+
     allTeachers: Array<Object>;
-    constructor(private sharedService: SharedService) {
+    constructor(private sharedService: SharedService,
+                private _cfr: ComponentFactoryResolver
+    ) {
         
     }
     ngOnInit(){
@@ -45,8 +52,77 @@ export class SettingsComponent {
             // setTimeout()
         }
     }
-    addTeacher(action){
-        this.openModal = true;
-        this.purpose = action;
+    addTeacher(){
+        let modalOptions = {
+            metaData: {
+              chaining: false,
+              labels: false,
+              setOnTop: true
+            },
+            header: 'Add new teacher',
+            body: [{
+                    type: 'text',
+                    label: 'Full name',
+                    placeholder: 'Full name',
+                    id: 'fullname'
+                },{
+                    type: 'text',
+                    label: 'Designation',
+                    placeholder: 'Designation',
+                    id: 'designation'
+                },{
+                    type: 'text',
+                    label: 'Subjects',
+                    placeholder: 'Subjects',
+                    id: 'subjects'
+                },{
+                    type: 'text',
+                    label: 'Departments',
+                    placeholder: 'Departments',
+                    id: 'departments'
+                }
+            ],
+            footer: [{
+                    type: 'button',
+                    label: 'Submit',
+                    id: 'submit',
+                    icon: 'fa fa-check'
+                },{
+                    type: 'button',
+                    label: 'Cancel',
+                    id: 'cancel',
+                    icon: 'fa fa-times'
+                }
+            ]
+          };
+          this.container.clear();
+          // check and resolve the component
+          let comp = this._cfr.resolveComponentFactory(ModalComponent);
+          // Create component inside container
+          let modalComponent = this.container.createComponent(comp);
+          
+          modalComponent.instance["_ref"] = modalComponent;
+          modalComponent.instance.options = modalOptions;
+          
+          
+          modalComponent.instance.output
+            .subscribe( output => {
+                if(output.get('status') != 'cancel'){
+                    let teacher = {
+                        fullname: output.get('fullname'),
+                        designation: output.get('designation'),
+                        subjects: output.get('subjects').split(','),
+                        departments: output.get('departments').split(',')  
+                    }
+                    this.sendTeacherDetails(`${TEACHER_BASE_URL}/add`, teacher)
+                }
+          });
+    }
+    sendTeacherDetails(URL, teacher){
+        this.sharedService.postCall(URL, teacher)
+            .subscribe(
+                console.log
+            ),
+            console.error
     }
 }
