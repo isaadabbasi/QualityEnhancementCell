dd6c515cb78c18a5028a58e5a8f5f0ee50f1764a
SMTaha <s.m.taha10@gmail.com>
2017-12-15 11:55:59 +0500

    Add model type safety

--- src/app/shared/models.ts
+++ src/app/shared/models.ts
@@ -1,17 +1,37 @@
 export interface StudentModel {
-    department: string,
+    id?:        string
+    fullname?:  string,
+    password?:  string
     rollnumber: string,
-    fullname?: string,
-    password?: string
-    id?: string
+    department: string,
 }
 
 export interface TeacherModel {
-    id: string,
-    fullname: string,
-    designation:string,
-    survey: Array<Object>,
-    surveys: Array<any>,
-    subjects: Array<string>,
-    departments: Array<string>
+    id:             string,
+    fullname:       string,
+    designation:    string,
+    surveys:        Array<any>,
+    survey:         Array<Object>,
+    subjects:       Array<string>,
+    departments:    Array<string>
+}
+
+export interface UserCredentialsModel {
+    rollnumber: string, 
+    password:   string, 
+    email:      string
+} 
+
+export interface TeacherListItem {
+    fullname    : string,
+    designation : string,
+    
+    surveys     : Array<Object>
+    subjects    : Array<string>,
+    departments : Array<string>,
+}
+
+export interface DeparmentsListItemModel{
+    value   : string,
+    name    : string
 }
